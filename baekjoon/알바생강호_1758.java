cd4cc8d088f129e546ce6460bd16977e1b63d79b
KIMHYOJUN <olzitoon@naver.com>
2023-01-10 13:34:23 +0900

    Feat. [ADD]Greedy

---
+++ baekjoon/알바생강호_1758.java
@@ -0,0 +1,27 @@
+package baekjoon;
+import java.util.*;
+import java.io.*;
+
+/**
+ * ans의 값의 범위를 정확히 인지하자
+ * 100000*100000=>1억이 넘는다 =Long으로 표현
+ */
+public class 알바생강호_1758 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Long ans = 0L;
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            al.add(Integer.parseInt(br.readLine()));
+        }
+        al.sort(Collections.reverseOrder());
+        for(int i=0;i<N;i++){
+            if(al.get(i)-i>0) ans += (al.get(i) - i);
+        }
+        bw.write(String.valueOf(ans));
+        bw.close();
+    }
+}
