9668ee36531c8dce9e2c9b836f0e708c4843a215
KIMHYOJUN <olzitoon@naver.com>
2023-01-10 18:59:23 +0900

    Feat. [ADD]Greedy

---
+++ baekjoon/동전0_11047.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class 동전0_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            al.add(Integer.parseInt(br.readLine()));
+        }
+        int ans = 0;
+        al.sort(Comparator.reverseOrder());
+        while(true){
+            int now = 0;
+            for(int i=0;i<n;i++){
+                if(k/al.get(i)>=1){
+                    ans += k / al.get(i);
+                    now = al.get(i);
+                    break;
+                }
+            }
+            k = k-(k/now)*now;
+            if(k==0) break;
+        }
+        bw.write(String.valueOf(ans));
+        bw.close();
+    }
+}
