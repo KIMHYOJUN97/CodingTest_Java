4af74a444b55fb900c57eca23bd2f2946f8cb744
KIMHYOJUN <olzitoon@naver.com>
2023-03-15 20:34:22 +0900

    Feat. [ADD]9Weeks-Two Pointer

--- baekjoon/Week9_TwoPointer/G4_15961_회전초밥.java
+++ baekjoon/Week9_TwoPointer/G4_15961_회전초밥.java
@@ -7,6 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+/**
+ * 슬라이딩 윈도우
+ * 초밥기계가 원형으로 이루어져 있기 때문에 end가 n까지의 범위가 아닌,
+ * n+k까지 범위 까지 가서 가짓수를 세어주어야 한다.
+ */
 public class G4_15961_회전초밥 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,13 +20,16 @@ public class G4_15961_회전초밥 {
         int d = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        int[] sushi = new int[n];
+        int[] sushi = new int[n+k];
         for (int i = 0; i < n; i++) {
             sushi[i] = Integer.parseInt(br.readLine());
         }
+        for (int i = n; i < n + k; i++) {
+            sushi[i] = sushi[i-n];
+        }
         int start=0,end=0,cnt=0,ans=0;
         Map<Integer, Integer> map = new HashMap<>();
-        while (end < n) {
+        while (end < n+k) {
             if(end-start<k){
                 if(map.getOrDefault(sushi[end],0)==0){
                     map.put(sushi[end],1);
