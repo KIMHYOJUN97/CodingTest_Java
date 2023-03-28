434c4eed6671c08e80d19442b44e806dca93da73
KIMHYOJUN <olzitoon@naver.com>
2023-03-28 23:27:19 +0900

    Feat. [ADD]10Weeks-Graph

---
+++ baekjoon/Week11_Graph/S1_2178_미로탐색.java
@@ -0,0 +1,13 @@
+package baekjoon.Week11_Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S1_2178_미로탐색 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+
+    }
+}
