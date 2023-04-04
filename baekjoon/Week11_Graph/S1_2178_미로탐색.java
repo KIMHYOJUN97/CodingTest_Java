68a5814499ec9d67f3d0114831b221608c0a9f28
KIMHYOJUN <olzitoon@naver.com>
2023-04-04 15:04:50 +0900

    Feat. [ADD]10Weeks-Graph

--- baekjoon/Week11_Graph/S1_2178_미로탐색.java
+++ baekjoon/Week11_Graph/S1_2178_미로탐색.java
@@ -3,11 +3,50 @@ package baekjoon.Week11_Graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class S1_2178_미로탐색 {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+        int[][] graph = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(String.valueOf(input.charAt(j)));
+            }
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0, 0));
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+            int x = now.x;
+            int y = now.y;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && graph[ny][nx] == 1) {
+                    graph[ny][nx] = graph[y][x]+1;
+                    q.add(new Node(nx, ny));
+                }
+            }
+        }
+        System.out.println(graph[n-1][m-1]);
+    }
 
+    public static class Node{
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
