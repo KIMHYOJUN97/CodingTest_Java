4b3296b88d09135de927f0014c244ccbdde6eb9c
KIMHYOJUN <olzitoon@naver.com>
2023-07-22 21:00:50 +0900

    Feat. [ADD] DP

---
+++ baekjoon/Week18/S3_17626_FourSquares.java
@@ -0,0 +1,29 @@
+package baekjoon.Week18;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S3_17626_FourSquares {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        int min = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            min = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                int tmp = i - j * j;
+                min = Math.min(min, dp[tmp]);
+            }
+            dp[i] = min + 1;
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
