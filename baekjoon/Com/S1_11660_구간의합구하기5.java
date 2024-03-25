2ce0490ca5cb7e9aba56e1d389282f490d4c2c49
KIMHYOJUN <olzitoon@naver.com>
2024-03-25 19:58:15 +0900

    feat. 누적합. 행,렬 정보 숙지하기.

--- baekjoon/Com/S1_11660_구간의합구하기5.java
+++ baekjoon/Com/S1_11660_구간의합구하기5.java
@@ -20,45 +20,29 @@ public class S1_11660_구간의합구하기5 {
         String[] nm = br.readLine().split(" ");
         int n = stoi(nm[0]);
         int m = stoi(nm[1]);
-        int[][] graph = new int[n][n];
-        int[][] preSum = new int[n][n];
+        int[][] graph = new int[n + 1][n + 1];
+        int[][] preSum = new int[n + 1][n + 1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             String[] input = br.readLine().split(" ");
-            for (int j = 0; j < n; j++) {
-                graph[i][j] = stoi(input[j]);
+            for (int j = 1; j <= n; j++) {
+                graph[i][j] = stoi(input[j-1]);
             }
         }
 
-        preSum[0][0] = graph[0][0];
-
-        for (int i = 1; i < n; i++) {
-            preSum[i][0] = preSum[i - 1][0] + graph[i][0];
-            preSum[0][i] = preSum[0][i - 1] + graph[0][i];
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] - preSum[i - 1][j - 1] + graph[i][j];
             }
         }
 
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                sb.append(preSum[i][j] + " ");
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
-
         for (int i = 0; i < m; i++) {
             String[] input = br.readLine().split(" ");
-            int x1 = stoi(input[0]) - 1;
-            int y1 = stoi(input[1]) - 1;
-            int x2 = stoi(input[2]) - 1;
-            int y2 = stoi(input[3]) - 1;
-            System.out.println(preSum[y2][x2] - preSum[y2][x1] - preSum[y1][x2] + preSum[y1][x1]);
+            int x1 = stoi(input[0]);
+            int y1 = stoi(input[1]);
+            int x2 = stoi(input[2]);
+            int y2 = stoi(input[3]);
+            System.out.println(preSum[x2][y2] - preSum[x1 - 1][y2] - preSum[x2][y1 - 1] + preSum[x1 - 1][y1 - 1]);
         }
     }
 
