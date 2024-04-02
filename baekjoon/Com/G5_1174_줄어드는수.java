131498d8aa4ca38f591e16a43a7c91fe6041048d
KHJ <olzitoon@naver.com>
2024-04-02 14:15:37 +0900

    feat: [G5] BACK TRACKING

---
+++ baekjoon/Com/G5_1174_줄어드는수.java
@@ -0,0 +1,13 @@
+package baekjoon.Com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class G5_1174_줄어드는수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
