292f6a48c7faaaa6afd2ac089bfd77e3f4071436
KHJ <olzitoon@naver.com>
2024-04-12 15:14:16 +0900

    feat: [G3] PRESUM

--- baekjoon/Com/G3_10986_나머지합.java
+++ baekjoon/Com/G3_10986_나머지합.java
@@ -8,18 +8,18 @@ import java.util.Map;
 
 /**
  * 수 N개 A1, A2, ..., AN이 주어진다. 이때, 연속된 부분 구간의 합이 M으로 나누어 떨어지는 구간의 개수를 구하는 프로그램을 작성하시오.
- *
+ * <p>
  * 즉, Ai + ... + Aj (i ≤ j) 의 합이 M으로 나누어 떨어지는 (i, j) 쌍의 개수를 구해야 한다.
- *
+ * <p>
  * 첫째 줄에 N과 M이 주어진다. (1 ≤ N ≤ 106, 2 ≤ M ≤ 103)
- *
+ * <p>
  * 둘째 줄에 N개의 수 A1, A2, ..., AN이 주어진다. (0 ≤ Ai ≤ 109)
- *
+ * <p>
  * 첫째 줄에 연속된 부분 구간의 합이 M으로 나누어 떨어지는 구간의 개수를 출력한다.
- *
+ * <p>
  * 5 3
  * 1 2 3 1 2
- *
+ * <p>
  * 7
  */
 public class G3_10986_나머지합 {
@@ -29,24 +29,20 @@ public class G3_10986_나머지합 {
 
         int n = stoi(s[0]);
         int m = stoi(s[1]);
-        int[] arr = new int[n];
-        int[] presum = new int[n];
-        s = br.readLine().split(" ");
-        for(int i=0; i<n; i++) {
-            arr[i] = stoi(s[i]);
-            presum[i] = i==0 ? arr[i] : presum[i-1] + arr[i];
-        }
-
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-
+        int[] arr = new int[n + 1];
+        int[] presum = new int[n + 1];
         long answer = 0;
-        for(int i=0; i<n; i++) {
-            int key = presum[i] % m;
-            int value = map.getOrDefault(key, 0);
-            answer += value;
-            map.put(key, value + 1);
+        s = br.readLine().split(" ");
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            if (stoi(s[i - 1]) % 3 == 0) answer++;
+            presum[i] = presum[i - 1] + stoi(s[i - 1]);
+            answer += map.getOrDefault(presum[i] % m, 0);
+
+            map.put(presum[i] % m, map.getOrDefault(presum[i] % m, 0) + 1);
+
         }
+
 
         System.out.println(answer);
 
