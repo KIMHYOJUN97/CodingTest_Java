5b33fef27d72980c407c246a8d62c53192f5d299
KHJ <olzitoon@naver.com>
2024-04-15 13:31:02 +0900

    feat: [G5] GREEDY

---
+++ baekjoon/Com/G5_21758_꿀따기.java
@@ -0,0 +1,17 @@
+package baekjoon.Com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class G5_21758_꿀따기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = stoi(br.readLine());
+        String[] s = br.readLine().split(" ");
+    }
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+}
