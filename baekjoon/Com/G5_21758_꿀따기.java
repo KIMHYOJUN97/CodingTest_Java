0970875ec848bca61919c339b7f0dc4efaecef17
KIMHYOJUN <olzitoon@naver.com>
2024-04-15 22:37:15 +0900

    feat: [G3] 누적합 + 그리디(케이스 계산)

--- baekjoon/Com/G5_21758_꿀따기.java
+++ baekjoon/Com/G5_21758_꿀따기.java
@@ -9,6 +9,26 @@ public class G5_21758_꿀따기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = stoi(br.readLine());
         String[] s = br.readLine().split(" ");
+        int[] arr = new int[n];
+        int[] presum = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = stoi(s[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (i == 0) presum[i] = arr[i];
+            else presum[i] = presum[i - 1] + arr[i];
+        }
+
+
+        int answer = 0;
+        for (int i = 1; i < n; i++) {
+            answer = Math.max(presum[i] - presum[0] + presum[n - 1] - presum[i - 1], answer);
+            answer = Math.max(presum[n - 1] * 2 - presum[i] * 2 + presum[i - 1] - presum[0], answer);
+            answer = Math.max(presum[i - 1] * 2 + presum[n - 2] - presum[i], answer);
+        }
+
+        System.out.println(answer);
+
     }
 
     static int stoi(String s) {
