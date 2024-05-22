7c081abecae39aec0318e500ddf97ddd5789e535
KHJ <olzitoon@naver.com>
2024-05-22 15:29:11 +0900

    [G5] DP

---
+++ baekjoon/Com/G5_9251_LCS.java
@@ -0,0 +1,30 @@
+package baekjoon.Com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열이 주어졌을 때, 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
+ *
+ * 예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
+ *
+ * 첫째 줄과 둘째 줄에 두 문자열이 주어진다. 문자열은 알파벳 대문자로만 이루어져 있으며, 최대 1000글자로 이루어져 있다.
+ *
+ * 첫째 줄에 입력으로 주어진 두 문자열의 LCS의 길이를 출력한다.
+ *
+ * ACAYKP
+ * CAPCAK
+ *
+ * 4
+ */
+public class G5_9251_LCS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+}
