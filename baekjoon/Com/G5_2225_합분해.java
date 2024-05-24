c7e6e2504a76f8c1d2eac446c55f5f025726533e
KIMHYOJUN <olzitoon@naver.com>
2024-05-24 23:34:16 +0900

    [feat] G5 [i-1][j], [i][j-1]

--- baekjoon/Com/G5_2225_합분해.java
+++ baekjoon/Com/G5_2225_합분해.java
@@ -16,6 +16,24 @@ public class G5_2225_합분해 {
         int n = stoi(s[0]);
         int k = stoi(s[1]);
 
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+            dp[i][1] = 1;
+        }
+
+        for (int i = 0; i <= k; i++) {
+            dp[1][i] = i;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= k; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
+            }
+        }
+
+        System.out.println(dp[n][k]);
     }
 
     static int stoi(String s) {
